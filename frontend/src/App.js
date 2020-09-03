7401dd68e28ca3a03264c3867eb7ce4434a25ad2
Chamon562 <Chamon562@gmail.com>
2020-09-03 15:38:37 -0700

    [components] Add navbar component and test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import './App.css';
+import Navbar from './components/Navbar'
 import Welcome from './components/Welcome'
 import About from './components/About'
 import Footer from './components/Footer'
+import './App.css';
 function App() {
   return (
     <div className="App">
+    <Navbar />
      <Welcome />
      <Footer />
     </div>
