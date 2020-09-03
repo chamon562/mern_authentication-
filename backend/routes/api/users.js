ff5f8fb151a39e719ed3202bc1e7912a1cf0a5d0
Chamon562 <Chamon562@gmail.com>
2020-09-03 14:15:55 -0700

    [users] Add /current route(private)

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -78,5 +78,13 @@ router.post('/login', (req, res) =>{
     })
 })
 
+// Get api/users/current (Private)
+router.get('/current', passport.authenticate('jwt', {session: false}), (req, res)  =>{
+    res.json({
+        id: req.user.id,
+        name: req.user.name,
+        email: req.user.email
+    })
+})
 
 module.exports = router;
